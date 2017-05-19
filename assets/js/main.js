a889586047fe8b5b285b185a7a8c8ac5793c872e
genarvaez <ge_naravez@hotmail.com>
2017-05-18 22:45:47 -0400

    ejercicio terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+
+var content = document.getElementById("box-image");
+pictures.forEach(function(e){
+var newImage = document.createElement("img");
+newImage.setAttribute("class", "image");
+newImage.setAttribute("src", e);
+content.appendChild(newImage);
+
+})
